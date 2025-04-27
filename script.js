0f1f136db1d4917d32d66ce9e6354e82bbcda563
joanna-laurel <191937222+joanna-laurel@users.noreply.github.com>
2025-04-27 16:36:39 -0400

    Get UI essentially functional
    The calculator works! You can do all the basic maths with it!
    
    There are a few small weird glitchy things, some design decisions to be
    made, and possible additional features. It should also be tested more
    extensively to make sure I accounted for various use instances.
    
    I also need to figure out how to batch process event listeners and have
    it actually work because all these individual ones are making the code
    twice as long as it could be and probably slowing it down a bunch.

--- script.js
+++ script.js
@@ -1,126 +1,191 @@
-
-// const one = document.getElementById("oneButton");
-// const two = document.getElementById("twoButton");
-// const three = document.getElementById("threeButton");
-// const four = document.getElementById("fourButton");
-// const five = document.getElementById("fiveButton");
-// const six = document.getElementById("sixButton");
-// const seven = document.getElementById("sevenButton");
-// const eight = document.getElementById("eightButton");
-// const nine = document.getElementById("nineButton");
-// const zero = document.getElementById("zeroButton");
+//TO DO
+//make sure dividing by 0 doesnt crash, have good message
+//add a +/- toggle button
+//add a percent button that makes it into a decimal
+//exponent button?
+//allow successive evaluations
+//figure out how to batch process event listeners 
+//fix screen overflow -- don't show whole problem or something trickier...
 
 
-// const plus = document.getElementById("plusButton");
-// const minus = document.getElementById("minusButton");
-// const times = document.getElementById("timesButton");
-// const dividedBy = document.getElementById("dividedByButton");
+
+
+
 const display = document.getElementById("displayDiv");
+
+const one = document.getElementById("oneButton");
+const two = document.getElementById("twoButton");
+const three = document.getElementById("threeButton");
+const four = document.getElementById("fourButton");
+const five = document.getElementById("fiveButton");
+const six = document.getElementById("sixButton");
+const seven = document.getElementById("sevenButton");
+const eight = document.getElementById("eightButton");
+const nine = document.getElementById("nineButton");
+const zero = document.getElementById("zeroButton");
+
+const plus = document.getElementById("plusButton");
+const minus = document.getElementById("minusButton");
+const times = document.getElementById("timesButton");
+const dividedBy = document.getElementById("dividedByButton");
+
 const decimal = document.getElementById("decimalButton");
 const clear = document.getElementById("clearButton");
 const backspace = document.getElementById("backspaceButton");
 const equals = document.getElementById("equalsButton");
 
-let firstNumber = [5];
-let operator = [""];
+let firstNumber = [];
+let operator = "";
 let secondNumber = [];
 let equalsSign = "";
 let solution = [];
 
-const num1 = firstNumber.length < 1 ? 
-  "" : parseFloat(firstNumber.slice().join(""));
-const num2 = secondNumber.length < 1 ?
-  "" : parseFloat(secondNumber.slice().join(""));
-const answer = solution.length < 1 ?
-  "" : parseFloat(solution.slice().join(""));
 
-display.textContent = (`${num1} ${operator[0]} ${num2} ${equalsSign} ${answer}`);
 
+getUserInput(); //RIDICULOUS BUT FUNCTIONAL
 function getUserInput() {
+  console.log("getting input");
+  function chooseOne() {addDigit("1")};
+  function chooseTwo() {addDigit("2")};
+  function chooseThree() {addDigit("3")};
+  function chooseFour() {addDigit("4")};
+  function chooseFive() {addDigit("5")};
+  function chooseSix() {addDigit("6")};
+  function chooseSeven() {addDigit("7")};
+  function chooseEight() {addDigit("8")};
+  function chooseNine() {addDigit("9")};
+  function chooseZero() {addDigit("0")};
 
-  document.querySelectorAll(".number").forEach(item => {
-    item.addEventListener("click", addDigit(item.textContent));
-  })
+  one.addEventListener("click", chooseOne);
+  two.addEventListener("click", chooseTwo);
+  three.addEventListener("click", chooseThree);
+  four.addEventListener("click", chooseFour);
+  five.addEventListener("click", chooseFive);
+  six.addEventListener("click", chooseSix);
+  seven.addEventListener("click", chooseSeven);
+  eight.addEventListener("click", chooseEight);
+  nine.addEventListener("click", chooseNine);
+  zero.addEventListener("click", chooseZero);
 
-  document.querySelectorAll(".operator").forEach(item => {
-    item.addEventListener("click", selectOperator(item.textContent));
-  })
+  console.log("hello")
 
-  decimal.addEventListener("click", addDecimal());
-  clear.addEventListener("click", clearEverything()); 
-  backspace.addEventListener("click", backspaceOnce()); 
-  equals.addEventListener("click", canWeOperate());
-    //update the display dynamically
-    //store the first number, operator, second number
-    //call the operate function operate( , , )
+  function chooseAddition() {selectOperator("+")};
+  function chooseSubtraction() {selectOperator("-")};
+  function chooseMultiplication() {selectOperator("*")};
+  function chooseDivision() {selectOperator("/")};
   
-  
+  plus.addEventListener("click", chooseAddition);
+  minus.addEventListener("click", chooseSubtraction);
+  times.addEventListener("click", chooseMultiplication);
+  dividedBy.addEventListener("click", chooseDivision);
+
+  function chooseDecimal() {addDecimal()}; 
+  function chooseClear() {clearEverything()};
+  function chooseBackspace() {backspaceOnce()};
+  function chooseEquals() {canWeOperate()}
+
+  decimal.addEventListener("click", chooseDecimal);
+  clear.addEventListener("click", chooseClear); 
+  backspace.addEventListener("click", chooseBackspace); 
+  equals.addEventListener("click", chooseEquals);
 }
 
-function addDigit(userDigit) {
-  if (operator[0] === "" && firstNumber.length < 8) {
-    firstNumber.push(userDigit);
+function updateDisplay() { //WORKS break up display though?
+  const num1 = firstNumber.length < 1 ? 
+    "" : firstNumber.slice().join("");
+  
+  const num2 = secondNumber.length < 1 ?
+    "" : secondNumber.slice().join("");
+  
+  const answer = solution.length < 1 ?
+    "" : solution.slice().join("");
+  
+  display.textContent = (`${num1} ${operator} ${num2} ${equalsSign} ${answer}`);
+    console.log("updating content")
   }
-  if (operator[0] !== "" && secondNumber.length < 8) {
+
+function addDigit(userDigit) { //WORKS
+  if (operator == "" && firstNumber.length < 8 && solution == "") {
+    firstNumber.push(userDigit);
+    console.log(firstNumber);
+  }
+  if (operator !== "" && secondNumber.length < 8 && solution == "") {
     secondNumber.push(userDigit);
   }
+  updateDisplay();
 }
 
-function addDecimal() {
-  if (operator[0] === "" && firstNumber.length < 8 
+function addDecimal() { //WORKS
+  console.log("adding decimal")
+  if (operator == "" && firstNumber.length < 8 && solution == ""
     && !firstNumber.includes(".")) {
-        firstNumber.push(".")};
+        firstNumber.push(".");
+  }
 
-  if (operator[0] !== "" && secondNumber.length < 8 
+  if (operator !== "" && secondNumber.length < 8 && solution == ""
     && !secondNumber.includes(".")) {
-        secondNumber.push(".")}; 
+        secondNumber.push(".");
+  }
+  updateDisplay();
 }
 
-function selectOperator(userOperator) {
-  if (operator[0] === "") {
-    operator[0] = userOperator;
+function selectOperator(userOperator) { //WORKS
+  if (operator == "") {
+    operator = userOperator;
   }
 
   if (solution.length > 0) {
-    firstNumber = solution.slice;
-    operator = [];
+    firstNumber = solution.slice();
+    console.log(firstNumber);
+    operator = userOperator;
     secondNumber = [];
     equalsSign = ""
+    solution = [];
+  }
+  updateDisplay();
+}
+
+function backspaceOnce() { //WORKS
+  if (solution.length < 1) {
+    secondNumber.length > 0 && equalsSign == "" ? 
+    secondNumber.pop() :
+    operator !== "" ? 
+    operator = "" :
+    firstNumber.pop();
+    updateDisplay();
   }
 }
 
-function backspaceOnce() {
-  secondNumber.length > 0 && solution.length == 0 ? 
-  secondNumber.pop() :
-  operator !== "" && solution.length == 0 ? 
-  operator.pop() :
-  firstNumber.length > 0 && solution.length == 0 ?
-  firstNumber.pop : null;
-}
-
-function clearEverything() {
+function clearEverything() { //WORKS
   firstNumber = [];
-  operator = [];
+  operator = "";
   secondNumber = [];
   equalsSign = "";
   solution = [];
-  display.textContent = "";
+  display.textContent = "more math please!";
 }
 
-function canWeOperate() {
+function canWeOperate() { //WORKS
   if (secondNumber.length > 0 && 
     !(secondNumber[0] === "." && secondNumber.length === 1)) {
     operate();
   }
 }
 
-function operate() {
-  solution = calculate(num1, operator[0], num2).split("");
+function operate() { //WORKS
+  console.log("operating")
+  let num1 = parseFloat(firstNumber.slice().join(""));
+  let num2 = parseFloat(secondNumber.slice().join(""));
+  console.log(num1, num2);
+  answer = calculate(num1, operator, num2);
+  answerRounded = Math.round(answer * 10000000) / 10000000;
+  solution = answerRounded.toString().split("");
+  console.log(solution);
   equalsSign = "=";
-  display.textContent = (`${num1} ${operator[0]} ${num2} ${equalsSign} ${solution.join}`);
+  updateDisplay();
 }
 
-function calculate(num1, operator, num2) { //tested, works
+function calculate(num1, operator, num2) { //WORKS
   return operator == "+" ? add(num1, num2) 
        : operator == "-" ? subtract(num1, num2) 
        : operator == "*" ? multiply(num1, num2) 
@@ -128,7 +193,7 @@ function calculate(num1, operator, num2) { //tested, works
        : "";
 };
 
-function infinityError() {
+function cantChooseZero() {
   display.textContent = "N000000000" //something to not reset right away
 }
 
@@ -145,11 +210,6 @@ function multiply(multiplier1, multiplier2) {
 }
 
 function divide(dividend, divisor) {
-  return divisor === 0 ? infinityError()
+  return divisor === 0 ? cantChooseZero()
     : dividend / divisor;
 }
-
-
-// console.log(operate(45, "*", 29)) works!!!
-
-
